0e3fd36873d110afc6558fff9d54be1a84bf3782
Mariel <Mariel.LatellaBarros@student.pxl.be>
2020-04-16 13:51:57 +0200

    Demo1 - Establish coonection (via app, not pom) and create a table in the database

--- src/main/java/be/pxl/ja2/jdbc/CreateTable.java
+++ src/main/java/be/pxl/ja2/jdbc/CreateTable.java
@@ -4,10 +4,11 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//Demo1
 public class CreateTable {
 
     public static void main(String[] args) {
+
         try(Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/musicdb?useSSL=false", "user", "password");
             Statement statement = conn.createStatement()) {
             statement.execute("CREATE TABLE contacts (id INTEGER NOT NULL AUTO_INCREMENT, name TEXT, phone INTEGER, email TEXT, PRIMARY KEY (id))");
